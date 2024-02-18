44587cf932e147b368ed5d46dcdf4d5d734cf562
Marianna Mazzette <20104120@mail.wit.ie>
2024-02-18 21:51:33 +0000

    Joi validation country

--- src/controllers/dashboard-controller.js
+++ src/controllers/dashboard-controller.js
@@ -1,4 +1,5 @@
 import { db } from "../models/db.js";
+import { CountrySpec } from "../models/joi-schemas.js";
 
 export const dashboardController = {
   index: {
@@ -15,6 +16,13 @@ export const dashboardController = {
   },
 
   addCountry: {
+    validate: {
+      payload: CountrySpec,
+      options: { abortEarly: false },
+      failAction: function (request, h, error) {
+        return h.view("dashboard-view", { title: "Add Country error", errors: error.details }).takeover().code(400);
+      },
+    },
     handler: async function (request, h) {
       const loggedInUser = request.auth.credentials;
       const newCountry = {
