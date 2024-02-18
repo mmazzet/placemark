2bcf2171b688570792e8f9d02bc439fcd372a61a
Marianna Mazzette <20104120@mail.wit.ie>
2024-02-18 19:34:59 +0000

    cookie deletion and env

--- src/server.js
+++ src/server.js
@@ -7,6 +7,7 @@ import Cookie from "@hapi/cookie";
 import { webRoutes } from "./web-routes.js";
 import { db } from "./models/db.js";
 import { accountsController } from "./controllers/accounts-controller.js";
+import dotenv from "dotenv";
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
@@ -31,8 +32,8 @@ async function init() {
   await server.register(Cookie);
   server.auth.strategy("session", "cookie", {
     cookie: {
-      name: "playtime",
-      password: "secretpasswordnotrevealedtoanyone",
+      name: process.env.COOKIE_NAME,
+      password: process.env.COOKIE_PASSWORD,
       isSecure: false,
     },
     redirectTo: "/",
@@ -50,5 +51,11 @@ process.on("unhandledRejection", (err) => {
   console.log(err);
   process.exit(1);
 });
+
+const result = dotenv.config();
+if (result.error) {
+  console.log(result.error.message);
+  process.exit(1);
+}
 
 init();
