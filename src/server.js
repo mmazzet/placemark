f66919609dc18995503fbcfe4a9fc77e16ce60eb
Marianna Mazzette <20104120@mail.wit.ie>
2024-03-09 17:05:31 +0000

    updated port hapi server

--- src/server.js
+++ src/server.js
@@ -35,8 +35,8 @@ const swaggerOptions = {
 
 async function init() {
   const server = Hapi.server({
-    port: 3000,
-    host: "localhost",
+    port: process.env.PORT || 3000,
+    host: "0.0.0.0",
   });
 
   await server.register(Inert);
